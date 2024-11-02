afb164d2637d6387db0616eac246e4889cf962d6
tkhongsap <ta.khongsap@gmail.com>
2024-11-02 14:41:14 +0700

    fix deployment config

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Ensure this matches your deployment settings
-  },
   server: {
     port: process.env.PORT || 3000,
-    host: true,
+    host: '0.0.0.0',
   },
+  preview: {
+    port: process.env.PORT || 3000,
+    host: '0.0.0.0',
+  }
 });
